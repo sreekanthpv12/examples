9a59a3508b3be9c80638b8c27f162d3d5f2107d2
sree320132 <sreekanthpv12@gmail.com>
2023-02-11 11:51:32 +0530

    some function added

--- list1.py
+++ list1.py
@@ -2,3 +2,9 @@ ls1=[1,2,3,4,5]
 print(ls1)
 ls2=["sree","vihan",1,2,3]
 print(ls2)
+def add():
+    a=int(input("enter the num1 "))
+    b=int(input("enter the num2"))
+    c=a+b
+    return c
+print(add())
