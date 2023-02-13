988edc654d01944604b3106e63802a7a85dadc52
sree320132 <sreekanthpv12@gmail.com>
2023-02-13 08:47:46 +0530

    deco changes added

--- list4.py
+++ list4.py
@@ -1,3 +1,14 @@
 
 li5=["jigu","2",3,4,5]
 print(li5)
+def outer(dec):
+    def inner(a):
+        a=5
+        return a
+    return inner
+@outer
+def deco(a):
+    
+    return a
+print(deco(4))
+        
